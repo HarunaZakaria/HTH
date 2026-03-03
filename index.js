0478da7ebcb74130a30976e6fe5cd743fed6746c
Harunzy <Harunzy55@gmail.com>
2026-03-03 11:02:07 -0800

    add js file

---
+++ index.js
@@ -0,0 +1,12 @@
+/* Scroll Animation */
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach((sec) => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 400;
+    if (top > offset) {
+      sec.classList.add("show");
+    }
+  });
+});
